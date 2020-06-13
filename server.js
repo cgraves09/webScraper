e1662665562b18d03de58f63fa0f6e2b7a6fc81d
cgraves09 <christopher.graves09@gmail.com>
2020-06-13 13:08:49 -0400

    changing port for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var exphbs = require('express-handlebars');
 var logger= require('morgan');
 var mongoose = require('mongoose');
 var MONGODB_URI = process.env.MONGODB_URI ||'mongodb://localhost/webScrapper'; 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var app = express();
 
 app.use(logger('dev'));
